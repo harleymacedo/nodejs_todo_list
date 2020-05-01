82a608195f70e590037e321ae24637830a76c4c7
Harley Macedo <harley.ip@gmail.com>
2020-05-01 13:40:36 -0300

    View Engine configurado

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ const handleBars = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 
-//app.set('view engine', {})
+app.engine('handlebars', handleBars({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
+app.use(express.static(__dirname + '/public'))
 
 app.get('/', (req, res) => {
-    res.send('Teste1')
+    res.send('login')
 })
 
 app.listen(3000, () => {
